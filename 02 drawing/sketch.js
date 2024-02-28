d4cd2ef8210a4996032a4880a8bef88010e4f593
wizzard17 <160177901+wizzard17@users.noreply.github.com>
2024-02-28 15:20:07 -0600

    drawing day 2
    getting loops working

--- 02 drawing/sketch.js
+++ 02 drawing/sketch.js
@@ -2,22 +2,75 @@
 // Rurik Lung
 // 2/27/2024
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+let xPos, yPos;   //declaration only...
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400 , 600);
+  xPos = [];  yPos = [];
+  initWithLoops(); //comment out for the on-your-own challenge
+  // xPos = [width*0.05, width*0.95,width*0.05,width*0.95];
+  // yPos = [height*0.05,height*0.05,height*0.95,height*0.95];
 }
+
+
 
 function draw() {
+  background(220);
+  cornersAndMouseLoop();
+}
 
-  background(220)
-  cornersAndMouse()
+function mousePressed(){
+  //this calls automatically on a mousePress
+  xPos.push(mouseX);
+  yPos.push(mouseY);
 }
+
+function cornersAndMouseLoop(){
+  // a hopefully slightly more elegant version...
+  let i = 0;
+  while(i < xPos.length){
+    let x = xPos[i];
+    let y = yPos[i];
+    circle(x,y,20);
+    line(x,y,mouseX,mouseY);
+    i++; 
+  }
+  circle(mouseX, mouseY, 20);
+}
+
 function cornersAndMouse(){
-  circle(0+width*.05,0+height*.05,50)
-  circle(0+width*.05,height-height*.05,50)
-  circle(width-width*.05,0+height*.05,50)
-  circle(width-width*0.05,height-height*0.05,50)
+  // draw some circles near each of the four corners
+  // and connect some lines from there to the mouse position
+  fill(255);
+  circle(width*0.05, height*0.05, 20);
+  circle(width*0.95, height*0.05, 20);
+  circle(width*0.05, height*0.95, 20);
+  circle(width*0.95, height*0.95, 20);
+  circle(mouseX, mouseY, 20);
+  //lines
+  line(width*0.05, height*0.05, mouseX,mouseY);
+  line(width*0.95, height*0.05,  mouseX,mouseY);
+  line(width*0.05, height*0.95,  mouseX,mouseY);
+  line(width*0.95, height*0.95,  mouseX,mouseY);
 }
+function initWithLoops(){
+  //lay down some initial circles, but using loops to do so.
+  
+  for(let x = 10; x<width; x+=20){
+    xPos.push(x); 
+    yPos.push(0);
+  }
+  for(let x = 10; x<width; x+=20){
+    xPos.push(x); 
+    yPos.push(height);
+  }
+  for(let y = 10; y<height; y+=20){
+    xPos.push(width); 
+    yPos.push(y);
+  }
+  for(let y = 10; y<height; y+=20){
+    xPos.push(0); 
+    yPos.push(y);
+  }
+}
+function 
