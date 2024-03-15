15bb89f2de993b0f882aabb27fed643bd0d8fbf1
wizzard17 <160177901+wizzard17@users.noreply.github.com>
2024-03-15 15:12:09 -0600

    day4

--- staircase/sketch.js
+++ staircase/sketch.js
@@ -12,8 +12,9 @@ let flagY
 let flagx
 let wide = .1
 let multiply=10
-
-
+let totalaverage
+let average
+let amoutmade=0
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -36,27 +37,38 @@ if(mouseIsPressed&&wide>1){
 function staircase(w,maxheight,filling){
   let flagheight=0// resets flag max height so it can find the new spot
   //loop to print the terain
+  
+  totalaverage=0
+  amoutmade=0
+  
   for (let time = totheleft; x < width; time+=.005){
     
-    if(x>0){
-      //makes the rectangle and height using noise
-      randomenumber=noise(time)
-      h=map(randomenumber,0,1,1,maxheight)
-      stroke(filling)
-      rect(x,y-h,w,h)
-      //keeps track of the heighst point
-      if (h>flagheight){
-        flagheight=h
-        flagY=height-h
-        flagx=x
-      }
-      
+    
+    
+    //makes the rectangle and height using noise
+    randomenumber=noise(time)
+    h=map(randomenumber,0,1,1,maxheight)
+    stroke(filling)
+    rect(x,y-h,w,h)
+    //keeps track of the heighst point
+    totalaverage+=h
+    amoutmade+=1
+    if (h>flagheight){
+      flagheight=h
+      flagY=height-h
+      flagx=x
     }
+      
+    
     //moves the x position to the right 
     x+=w
   }
+  average=totalaverage/amoutmade
+  rect(0,height-average,width,20)
+  
+  console.log(totalaverage)
   x=0
-  totheleft+=.05
+  totheleft+=.5
   
 }
 //prints a flag 
