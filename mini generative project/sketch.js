8ac847c218cfe3a243997e3408d9bc761b852d80
wizzard17 <160177901+wizzard17@users.noreply.github.com>
2024-04-08 14:18:49 -0600

    mini poject finished forgot to uplouad

--- mini generative project/sketch.js
+++ mini generative project/sketch.js
@@ -1,47 +1,57 @@
-// Project Title
-// Your Name
-// Date
+// art cpoying project 
+// Rurik Lung
+// 3/28/2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - describe what you did to take this project "above and beyond"(nothing)
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(220);
+  rectMode(CENTER)
   fill("grey")
   noStroke()
-  rectCircle()
+  rectCircle(width/2,height/2)
  
 }
 
 function draw() {
-  ;
-  
-  
 }
-function rectCircle(){
-  translate(width/2,height/2)
+function rectCircle(centerX,centerY){
+
+  // makes it center
+  translate(centerX,centerY);
+
   for (let i = 0;i<300;i++){
-    let x = random(-200,200)
-    let y = random(-200,200)
+    let x = random(-200,200);
+    let y = random(-200,200);
     let range =Math.sqrt((x*x)+(y*y), 2);
-    let wide
-    let high
+
+    //varuables for shape size
+    let wide;
+    let high;
+
+    //cheacks if the shape it wants to make is in the circle area
     if(range <200){
       let correctsize=false;
+
+      //used to check if the shape it wants to make is rectangle or square loops till it is one
       while(correctsize===false){
-        wide = random(5,35)
-        high = random(5,35)
+        wide = random(5,35);
+        high = random(5,35);
+        //shape check
         if((wide<=7&&high>=10)||(high<=7&&wide>=10)||(wide<=10&&high<=10)){
           correctsize=true;
         }
       }
-      rect(x,y,wide,high)
+      
+      rect(x,y,wide,high);
     }
+
+    //if it isnt in cercle area it will try again by making the queu go back by one
     else{
-      i-=1
+      i-=1;
     }
-    
-   
   }
 }
